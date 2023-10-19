a61727d50ef0d233f27b2d65be87f7e1561c139c
Alex Bergelin <the_bergelin@hotmail.co.uk>
2023-10-19 14:23:28 +0100

    Add route for shoppinglist module

--- src/app/shopping-list/shopping-list.module.ts
+++ src/app/shopping-list/shopping-list.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
-import { ShoppingEditComponent } from './shopping-edit/shopping-edit.component';
-import { ShoppingListComponent } from './shopping-list.component';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { ShoppingEditComponent } from './shopping-edit/shopping-edit.component';
+import { ShoppingListComponent } from './shopping-list.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [ShoppingEditComponent, ShoppingListComponent],
-  imports: [FormsModule, BrowserModule],
+  imports: [
+    FormsModule, //Importing as we use template driven forms in this module
+    BrowserModule, //Importing ngFor etc
+    //Small module so adding route here
+    RouterModule.forChild([
+      { path: 'shopping-list', component: ShoppingListComponent },
+    ]),
+  ],
 })
 export class ShoppingListModule {}
